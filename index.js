5cd2fda01f3d8194cbb842f504a92619689b5b40
Veniche <brianchristian555@gmail.com>
2025-12-25 19:22:40 +0700

    test

--- index.js
+++ index.js
@@ -774,7 +774,7 @@ async function sendExpiryReminderEmail({
     from_email: process.env.MAIL_FROM_EMAIL,
     recipient: email,
     subject: '⚠️ Reminder: Keanggotaan CTA Anda Akan Habis Besok',
-    content: htmlContent,
+    content: 'test',
     attach1: '',
     attach2: '',
     attach3: '',
@@ -1067,7 +1067,7 @@ async function runExpiryCheck() {
 
 // Schedule daily run (default: 5:00 AM UTC; for UTC+7, that's 12:00 PM)
 cron.schedule("0 5 * * *", runExpiryCheck);
-cron.schedule("20 12 * * *", runExpiryReminder);
+cron.schedule("30 12 * * *", runExpiryReminder);
 
 // Temporary test API to run expiry check on demand (protected)
 app.post('/run-expiry-check', async (req, res) => {
